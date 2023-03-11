11b14d3b7a3c0718d46ab2bbd1bd54820b87b719
killian31 <80256683+killian31@users.noreply.github.com>
2023-03-11 12:54:00 +0000

    feat: training script

---
+++ scripts/train_model.py
@@ -0,0 +1,31 @@
+import sys
+
+import torch
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+
+sys.path.append("../")
+from models.UNet import SimpleUnet
+from scripts.train import train
+from utils.datasets import load_transformed_flowers
+from utils.util import linear_beta_schedule
+
+if __name__ == "__main__":
+    if torch.cuda.is_available():
+        device = "cuda"
+    else:
+        device = "cpu"
+
+    img_size = 64
+    data = load_transformed_flowers(img_size)
+    batch_size = 128
+    dataloader = DataLoader(data, batch_size=batch_size, shuffle=True, drop_last=True)
+    T = 300
+    betas = linear_beta_schedule(timesteps=T)
+    model = SimpleUnet()
+    optimizer = Adam(model.parameters(), lr=1e-3)
+    epochs = 100
+
+    model, losses = train(
+        optimizer, epochs, device, dataloader, batch_size, T, model, img_size, betas
+    )
--- utils/util.py
+++ utils/util.py
@@ -64,25 +64,6 @@ def forward_diffusion_sample(x_0, t, betas, device="cpu"):
     ) + sqrt_one_minus_alphas_cumprod_t.to(device) * noise.to(device), noise.to(device)
 
 
-def load_transformed_dataset(img_size):
-    data_transforms = [
-        transforms.Resize((img_size, img_size)),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),  # Scales data into [0,1]
-        transforms.Lambda(lambda t: (t * 2) - 1),  # Scale between [-1, 1]
-    ]
-    data_transform = transforms.Compose(data_transforms)
-
-    train = torchvision.datasets.StanfordCars(
-        root=".", download=True, transform=data_transform
-    )
-
-    test = torchvision.datasets.StanfordCars(
-        root=".", download=True, transform=data_transform, split="test"
-    )
-    return torch.utils.data.ConcatDataset([train, test])
-
-
 def get_loss(model, x_0, t, device):
     x_noisy, noise = forward_diffusion_sample(x_0, t, device)
     noise_pred = model(x_noisy, t)
