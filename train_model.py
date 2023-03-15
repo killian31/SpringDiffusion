39a56be85ed7bf869b4437fc884d9b46f969b351
killian31 <80256683+killian31@users.noreply.github.com>
2023-03-15 21:34:53 +0100

    chore: added arg parse

--- train_model.py
+++ train_model.py
@@ -1,7 +1,7 @@
+import argparse
 import os
 import sys
 
-import matplotlib.pyplot as plt
 import torch
 from torch.optim import Adam
 from torch.utils.data import DataLoader
@@ -12,25 +12,48 @@ from scripts.train import train
 from utils.datasets import load_transformed_flowers
 from utils.util import linear_beta_schedule
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--img-size", type=int, default=128, help="size of output images")
+parser.add_argument("--batch-size", type=int, default=32, help="size of the batches")
+parser.add_argument(
+    "--sampling-steps", type=int, default=1000, help="number of sampling steps"
+)
+parser.add_argument(
+    "--epochs", type=int, default=20, help="number of epochs to train for"
+)
+parser.add_argument("--lr", type=float, default=0.001, help="adam: learning rate")
+parser.add_argument(
+    "--checkpoint",
+    type=str,
+    default=None,
+    help="model checkpoint to resume training from",
+)
 if __name__ == "__main__":
     if torch.cuda.is_available():
         device = "cuda"
     else:
         device = "cpu"
-
-    img_size = 64
-    data = load_transformed_flowers(img_size)
-    batch_size = 128
-    dataloader = DataLoader(data, batch_size=batch_size, shuffle=True, drop_last=True)
-    T = 300
-    betas = linear_beta_schedule(timesteps=T)
+    args = parser.parse_args()
+    data = load_transformed_flowers(args.img_size)
+    dataloader = DataLoader(
+        data, batch_size=args.batch_size, shuffle=True, drop_last=True
+    )
+    betas = linear_beta_schedule(timesteps=args.sampling_steps)
     model = SimpleUnet()
-    if os.path.exists("./weights/weights.pt"):
-        model.load_state_dict(torch.load("./weights/weights.pt"))
+    if args.checkpoint is not None:
+        model.load_state_dict(torch.load(args.checkpoint))
     else:
         os.makedirs("./weights", exist_ok=True)
-    optimizer = Adam(model.parameters(), lr=1e-3)
-    epochs = 10
+    optimizer = Adam(model.parameters(), lr=args.lr)
     model, losses = train(
-        optimizer, epochs, device, dataloader, batch_size, T, model, img_size, betas
+        optimizer,
+        args.epochs,
+        device,
+        dataloader,
+        args.batch_size,
+        args.sampling_steps,
+        model,
+        args.img_size,
+        betas,
     )
