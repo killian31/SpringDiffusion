32ae93df3c2f3cef8aaa901bbdb7420db6085b96
Killian Steunou <80256683+killian31@users.noreply.github.com>
2023-05-05 18:26:24 +0200

    feat: colab support

--- train_model.py
+++ train_model.py
@@ -29,6 +29,7 @@ parser.add_argument(
     default=None,
     help="model checkpoint to resume training from",
 )
+parser.add_argument("--use_colab", type="store_true", default=False)
 parser.add_argument("--gpuid", type=int, default=0, help="gpu id")
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -57,4 +58,5 @@ if __name__ == "__main__":
         model,
         args.img_size,
         betas,
+        args.use_colab
     )
